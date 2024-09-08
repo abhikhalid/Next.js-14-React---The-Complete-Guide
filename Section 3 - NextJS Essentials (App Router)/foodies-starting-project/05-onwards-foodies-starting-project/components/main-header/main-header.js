eb6c1eba0caa752024c10f975f169e68da3aea6b
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-08 21:06:19 +0600

    Using Client Components Efficiently

--- Section 3 - NextJS Essentials (App Router)/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ Section 3 - NextJS Essentials (App Router)/foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,10 +1,16 @@
+// 'use client'
+
 import Link from "next/link";
 import logoImg from '@/assets/logo.png';
 import classes from '@/components/main-header/main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "./nav-link";
+// import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+    // const path = usePathname();
+
     return(
     <>
     <MainHeaderBackground />
@@ -18,10 +24,15 @@ export default function MainHeader() {
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href="/meals">Browse Meals</Link>
+                            {/* <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link> */}
+
+                            <NavLink href="/meals" >Browse Meals</NavLink>
                 </li>
                 <li>
-                    <Link href="/community">Foodies Community</Link>
+                            {/* <Link href="/community" className={path ==='/community' ? classes.active : undefined}>Foodies Community</Link> */}
+                            <NavLink href="/community">
+                                Foodies Community
+                            </NavLink>
                 </li>
             </ul>
         </nav>
