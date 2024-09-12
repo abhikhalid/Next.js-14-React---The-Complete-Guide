1513656172b7875e367a352029846fd36a0974c4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-12 21:14:50 +0600

    Server vs Client Components
    note: try to make client component as small as possible

--- Section 4/01-starting-project/components/main-header.js
+++ Section 4/01-starting-project/components/main-header.js
@@ -1,11 +1,7 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
-    const path = usePathname();
-
     return (
         <header id="main-header">
             <div id="logo">
@@ -16,16 +12,10 @@ export default function MainHeader() {
             <nav>
                 <ul>
                     <li>
-                        <Link
-                            href="/news"
-                            className={path.startsWith('/news') ? 'active' : undefined}
-                        >News</Link>
+                      <NavLink href="/news">News</NavLink>
                     </li>
                     <li>
-                        <Link
-                            href="/archive"
-                            className={path.startsWith('/archive') ? 'active' : undefined}
-                        >Archive</Link>
+                        <NavLink href="/archive">Archive</NavLink>
                     </li>
 
                 </ul>
