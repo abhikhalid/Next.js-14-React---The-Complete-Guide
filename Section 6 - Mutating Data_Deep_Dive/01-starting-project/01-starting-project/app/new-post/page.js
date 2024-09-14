ee6cc6c6356d3081842b998552a1559235388074
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-14 14:00:06 +0600

    Validating User Input With Help Of The useFormState Hook

--- Section 6 - Mutating Data_Deep_Dive/01-starting-project/01-starting-project/app/new-post/page.js
+++ Section 6 - Mutating Data_Deep_Dive/01-starting-project/01-starting-project/app/new-post/page.js
@@ -1,3 +1,6 @@
+'use client'; //For using useFormState, but this component will throw an error, because we are using server component inside a client component. By writing 'use client' at the top of the file, we are saying that, all of the code of this file should be rendered on the client. (Browser)
+
+import { useFormState } from 'react-dom';
 import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
@@ -10,7 +13,25 @@ export default function NewPostPage() {
     const image = formData.get('image');
     const content = formData.get('content');
 
-    console.log(title, image, content);
+    // console.log(title, image, content);
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required");
+    }
+    
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required");
+    }
+
+    if (!image) {
+      errors.push("Image is required");
+    }
+    
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -22,10 +43,13 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
+  //just like useState hook
+  const [state,formAction] = useFormState(createPost,{});
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
