a3efcbdd3f1866667754b907f85d0c223f36a0e4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-14 12:35:48 +0600

    Setting Up A Form Action

--- Section 6 - Mutating Data_Deep_Dive/01-starting-project/01-starting-project/app/new-post/page.js
+++ Section 6 - Mutating Data_Deep_Dive/01-starting-project/01-starting-project/app/new-post/page.js
@@ -7,6 +7,8 @@ export default function NewPostPage() {
     const image = formData.get('image');
     const content = formData.get('content');
 
+    console.log(title, image, content);
+
     storePost({
       imageUrl: '',
       title,
