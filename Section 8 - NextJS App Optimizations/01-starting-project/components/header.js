79de9ed8d8ea9a4d69383b29ae5663216749294b
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2024-09-15 12:23:16 +0600

    Controlling the Image Size

--- Section 8 - NextJS App Optimizations/01-starting-project/components/header.js
+++ Section 8 - NextJS App Optimizations/01-starting-project/components/header.js
@@ -4,12 +4,17 @@ import Link from 'next/link';
 
 export default function Header() {
   console.log(logo);
-  
+
   return (
     <header id="main-header">
       <Link href="/">
         {/* { * <img src={logo.src} alt="Mobile phone with posts feed on it" /> */} 
-        <Image src={logo} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          // sizes='10vw'
+          alt="Mobile phone with posts feed on it" />
       </Link>
       <nav>
         <ul>
